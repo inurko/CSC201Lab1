e460b885b4f4945b37fad7686ee04d20ca16f57b
Ilan Nurko <ilannurko@Ilans-MBP.dhcp.wfu.edu>
2022-01-26 20:40:11 -0500

    aslkjs

--- src/CommandProcessor.java
+++ src/CommandProcessor.java
@@ -1,5 +1,11 @@
+
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * This class read input files and parses each line of commands.
@@ -12,7 +18,7 @@ public class CommandProcessor {
      * Instantiates a new command processor
      */
     public CommandProcessor(String fileName) throws FileNotFoundException {
-        World world = new World();
+        //world = new World();
         run(new File(fileName));
     }
 
@@ -20,6 +26,47 @@ public class CommandProcessor {
      * Read input file and parse each line of commands
      */
     public void run(File myFile) throws FileNotFoundException {
+
+
+        /*
+        String test = "1 2    23 4 5 ";
+        String result = "";
+        String input = "";
+        Scanner testS = new Scanner(test);
+
+        while (testS.hasNext()){
+             input = testS.next();
+            if(!input.equals(" ")){
+                result += input;
+            }
+        }
+        System.out.println("result: " + result);
+
+         */
+
+        FileInputStream file  = new FileInputStream(myFile);
+        Scanner scnr = new Scanner(file);
+        Scanner argParser;
+        String input;
+        ArrayList <String> args = new ArrayList<>();
+
+        while (scnr.hasNext()){
+            String argLine = scnr.nextLine(); //takes in first line from arg file
+
+            argParser = new Scanner(argLine); //will parse through the line to get arguments
+
+            while (argParser.hasNext()){
+            input = argParser.next();
+
+                if (! input.equals(" ")){
+                    args.add(input);
+                    System.out.println(input);
+                }
+
+
+            }
+
+       }
 
     }
 }
--- src/Rectangle1.java
+++ src/Rectangle1.java
@@ -2,7 +2,7 @@
  * This is your main class
  * Bonus: YES or NO
  */
-
+import java.util.Scanner;
 import java.io.FileNotFoundException;
 
 
@@ -13,11 +13,14 @@ public class Rectangle1 {
      * The main method.
      */
     public static void main(String[] args) throws FileNotFoundException {
+
         if(args.length != 1){
             System.err.println("Argument count is invalid: [" + args.length
                     + "]");
             System.exit(0);
         }
-        new CommandProcessor(args[0]);
+
+      new CommandProcessor(args[0]);
+
     }
 }
